182f3390b075c2fe2e132a8661788a8547e2647e
itsmohsinnadaf <itsmohsinnadaf@gmail.com>
2026-04-20 18:07:40 +0530

    Update RootFolders.jsx

--- Cinesphere/src/components/folders/RootFolders.jsx
+++ Cinesphere/src/components/folders/RootFolders.jsx
@@ -1,14 +1,7 @@
 // src/components/folders/RootFolders.jsx
+import LoadingScreen from "../common/LoadingScreen";
 
 export default function RootFolders({ folders, loading, error, onRootClick }) {
-  if (loading && !folders.length) {
-    return (
-      <section className="cs-left">
-        <h2 className="cs-section-title">Library</h2>
-        <p className="cs-section-subtitle">Loading from OneDrive...</p>
-      </section>
-    );
-  }
 
   if (error && !folders.length) {
     return (
